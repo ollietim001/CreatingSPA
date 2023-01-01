a7bb158edc8a755db651d00467ea6b2487a21824
Oliver Timothy <oliver.timothy@students.plymouth.ac.uk>
2023-01-01 18:58:13 +0000

    added SPA

---
+++ SPA/wwwroot/js/spa.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#account').click(function () {
+        $('#page-content-wrapper').load('account.html');
+    })
+});
+$(document).ready(function () {
+    $('#index').click(function () {
+        $('#page-content-wrapper').load('indexPanel.html');
+    })
+});
+$(document).ready(function () {
+    $('#login').click(function () {
+        $('#whole-page-content-wrapper').load('login.html');
+    })
+});
